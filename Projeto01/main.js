aa06f7d99f4d367284c204e90ae94207374697ac
Jonas Macedo <jonasmdcarvalho@gmail.com>
2023-07-14 15:49:22 -0300

    Testando o HTMLConnectio e NodeList

--- Projeto01/main.js
+++ Projeto01/main.js
@@ -1,16 +1,13 @@
 
-//Retornando todas as imagens
-var imagens = document.querySelectorAll('img')
-console.log(imagens)
-
 /* QuerySelecetorAll: ira retornar um NodeList que é estatico, se houver alterações posteriores 
 no elmento nao ira constar. */
-
-var gridSectionHTMLConnection = document.getElementsByClassName('sectionAnimais')
-console.log(gridSectionHTMLConnection); //HTMLConnection: dinamico.
-
 var gridSectionNode = document.querySelectorAll('describ')
 console.log(gridSectionNode); //NodeList: estatico.
+
+/* GetElements: retorna um HTMLCollection, a lista retornada é dinamica, significa que se 
+elementos forem adicionados, ela será automaticamente atualizada */
+var gridSectionHTMLConnection = document.getElementsByClassName('sectionAnimais')
+console.log(gridSectionHTMLConnection); //HTMLConnection: dinamico.
 
 /*QuerySelector: é um seletor geral unico, retorna o primeiro elemento que combinar com seu 
 seletor CSS. */
@@ -20,7 +17,24 @@ console.log(firstH2)
 var lastP= document.querySelector('p:last-child')
 console.log(lastP)
 
-/* GetElements: retorna um HTMLCollection, a lista retornada está ao vivo, significa que se 
-elementos forem adicionados, ela será automaticamente atualizada */
+//Retornando todas as imagens.
+var imagens = document.querySelectorAll('img')
+console.log(imagens)
+
+//Retornando todas as imagens, que comecam com a palavra imagem.
+var imgs = document.querySelectorAll('img[src^="./img/imagem"]')
+console.log(imgs);
+
+//Selecionando links internos com "#"
+var linksInternos = document.querySelectorAll('[href^="#"]')
+console.log(linksInternos);
+
+//Selecionando primeiro H2
+var h2Animais = document.querySelectorAll('h2')
+console.log(h2Animais[0]);
+
+//Selecionando ultimo paragrafo.
+var pS = document.querySelectorAll('p')
+console.log(pS[--pS.length]);
 
 
