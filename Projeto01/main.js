70bc712d5e6c207b3489ff6c63091bc6154a1463
Jonas Macedo <jonasmdcarvalho@gmail.com>
2023-07-20 10:12:03 -0300

    Exemplos de ForEach e ArrowFunction

--- Projeto01/main.js
+++ Projeto01/main.js
@@ -1,16 +1,23 @@
 
-/* QuerySelecetorAll: ira retornar um NodeList que é estatico, se houver alterações posteriores 
-no elmento nao ira constar. */
+/* QuerySelecetorAll: 
+    - ira retornar um NodeList que é estatico, se houver alterações posteriores 
+    - no elmento nao ira constar. 
+*/
 var gridSectionNode = document.querySelectorAll('describ')
 console.log(gridSectionNode); //NodeList: estatico.
 
-/* GetElements: retorna um HTMLCollection, a lista retornada é dinamica, significa que se 
-elementos forem adicionados, ela será automaticamente atualizada */
+/* GetElements: 
+    - retorna um HTMLCollection, a lista retornada é dinamica, significa que se 
+    - elementos forem adicionados, ela será automaticamente atualizada! 
+*/
+
 var gridSectionHTMLConnection = document.getElementsByClassName('sectionAnimais')
 console.log(gridSectionHTMLConnection); //HTMLConnection: dinamico.
 
-/*QuerySelector: é um seletor geral unico, retorna o primeiro elemento que combinar com seu 
-seletor CSS. */
+/* QuerySelector: 
+    - é um seletor geral unico, retorna o primeiro elemento que combinar com seu  seletor CSS. 
+*/
+
 var firstH2 = document.querySelector('h2')
 console.log(firstH2)
 
@@ -41,30 +48,31 @@ console.log(
 "///-------------------///------------------- FOREACH --------------------///-------------------///-------------------"
 )
 
-//Constantemente vamos selecionar uma lista de itens do dom. A melhor forma para interagirmos com os mesmos é utilizando o método forEach.
+/* FOREACH E ARRAY
+    - forEach é um método de Array, alguns objetos array-like possuem este método. 
+    - Caso não possua, o ideal é transforma-los em uma array.
+*/ 
+
 
 const imagensFor = document.querySelectorAll('img')
-
 imagensFor.forEach(function(item,index, array){
     
     /*O primeiro parâmetro é o callback, ou seja, oque sera retornado.
     a função que será ativada a cada item. 
     Esse função pode receber três parâmetros: valorAtual, index e array;*/
     
-    console.log(item) // item atual no loop.
-    console.log(index) // numero do index.
-   
+    console.log(item,index) // item atual no loop, e numero do index.
+    
 });
 
-
-//FOREACH E ARRAY
-// forEach é um método de Array, alguns objetos array-like possuem este método. 
-// Caso não possua, o ideal é transformá-los em uma array.
+//Constantemente vamos selecionar uma lista de itens da DOM.
 
 var titulos = document.getElementsByClassName('titulo')
 console.log(titulos)
 
+// A melhor forma para interagirmos com os mesmos é utilizando o método forEach.
 const arrayTitulos = Array.from(titulos) // Se comentar dara erro dizendo que nao é um array.
+
 arrayTitulos.forEach(function(item){
     console.log(item);
 })
@@ -76,5 +84,23 @@ tagsP.forEach((tag)=>{
 })
 
 //Retornando cada conteudo dos paragrafos
+tagsP.forEach((item) => {
+    console.log(item.innerText)
+});
+
+const itensMenu = document.querySelectorAll('.menu a')
+
+console.log(itensMenu)
+
+// adicionando a classe 'ATIVA' a todas os itens do menu.
+itensMenu.forEach((item)=>{
+    item.classList.add('ativo')
+    console.log(item)
+})
+
+
+// deixando a classe 'ATIVA' apenas no primeiro item do menu.
+itensMenu.forEach((itemRemove,ind)=>(ind == 0 )? itemRemove.classList.add('ATIVO') : itemRemove.classList.remove('ativo'))
+
 
 
