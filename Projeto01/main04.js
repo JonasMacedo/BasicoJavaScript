c1659be7ae6e590940ea573776c675f9d96d5e60
Jonas Macedo <jonasmdcarvalho@gmail.com>
2023-07-31 17:16:57 -0300

    Adicionando elemento na Dom e Clonagem

--- Projeto01/main04.js
+++ Projeto01/main04.js
@@ -2,6 +2,7 @@ console.log(
 "///-------------------///------------------- Navegacao de elementos DOM --------------------///------------------"
 )
 
+
 // TRASVERSING
 // Consiste em navegar pelo DOM, utilizando suas propriedades e métodos.
 
@@ -35,3 +36,28 @@ const titulo = contat.querySelector('.titulo');
 
 // Move o elemento/tag para o final da possição dentro da arvore de elementos.
 contat.insertBefore(lista, titulo); // Move apenas elementos filhos.
+
+//Adicionando um elemento com JS no DOM.
+
+//Denindo o tipo do elemento.
+const novoH1 = document.createElement('h1'); 
+
+//Insereindo conteudo.
+novoH1.innerText='Novo titulo' 
+
+//Inserindo classe de estilizacao.
+novoH1.classList.add('titulo')
+
+//Adicionando o elemento na DOM.
+const mapa = document.querySelector('.mapa')
+
+
+
+
+//Clonando Elementos da DOM.
+const tituloPrincipal = document.querySelector('h1')
+
+const cloneTituloPrincipal = tituloPrincipal.cloneNode(true)
+console.log(tituloPrincipal)
+console.log(cloneTituloPrincipal)
+tituloPrincipal.appendChild(cloneTituloPrincipal)
