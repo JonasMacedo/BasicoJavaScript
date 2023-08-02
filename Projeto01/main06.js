c126e843e2fbf2a9c4b5a3fd776cd90072d94edd
Jonas Macedo <jonasmdcarvalho@gmail.com>
2023-08-02 17:28:35 -0300

    Adicionando Accordion List

---
+++ Projeto01/main06.js
@@ -0,0 +1,8 @@
+
+/*
+    Accordion List
+    - Muito usado em sites resposniveis.
+    - Basicamentente uma lista que abre e fecha, lembrando o instrumento muscial 
+    - arcodeon/sanfona dai o nome Accordion.
+*/
+
