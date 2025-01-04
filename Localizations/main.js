164b17964e1fbcc7bb1980d4ed19cdd20e070033
Jonas <jonasmdcarvalho@gmail.com>
2025-01-04 19:15:32 -0300

    Coletando localizacao com JS

---
+++ Localizations/main.js
@@ -0,0 +1,15 @@
+
+let h2 = document.querySelector('h2')
+
+function localizationSucess(orig) {
+    console.log(orig.coords.latitude, orig.coords.longitude)
+    h2.textContent = `Latitude: ${orig.coords.latitude}, Longitude: ${orig.coords.longitude}`
+}
+
+function locNegada(orig) {
+    h2.textContent = `Não liberada a localização!!`
+}
+
+// Obtendo localizacao atual.
+navigator.geolocation.getCurrentPosition(localizationSucess, locNegada)
+
